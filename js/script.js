b5f7dcdc36ad0913ff085a0d8ad07efbb861cd22
Luca-Rubino <lucasrubino92@gmail.com>
2024-05-23 20:10:26 +0200

    Scrivere soluzione di Slack-1 utilizzando il metodo .map

--- js/script.js
+++ js/script.js
@@ -11,80 +11,95 @@
 
 console.log('Snack 1')
 
-// creo gli array con i dati richiesti in forma di stringhe
-const bradPitt = ['tableName: Tavolo Vip', 'guestName: Brad Pitt', 'place: 1']
-// console.log(bradPitt)
-const johnnyDepp = ['tableName: Tavolo Vip', 'guestName: Johnny Depp', 'place: 2']
-// console.log(johnnyDepp)
-const ladyGaga = ['tableName: Tavolo Vip', 'guestName: Lady Gaga', 'place: 3']
-// console.log(ladyGaga)
-const cristianoRonaldo = ['tableName: Tavolo Vip', 'guestName: Cristiano Ronaldo', 'place: 4']
-// console.log(cristianoRonaldo)
-const georginaRodrigue = ['tableName: Tavolo Vip', 'guestName: Georgina Rodriguez', 'place: 5']
-// console.log(georginaRodrigue)
-const chiaraFerragni = ['tableName: Tavolo Vip', 'guestName: Chiara Ferragni', 'place: 6']
-// console.log(chiaraFerragni)
-const georgeClooney = ['tableName: Tavolo Vip', 'guestName: George Clooney', 'place: 7']
-// console.log(georgeClooney)
-const amalClooney = ['tableName: Tavolo Vip', 'guestName: Amal Clooney', 'place: 8']
-// console.log(amalClooney)
-const fedez = ['tableName: Tavolo Vip', 'guestName: Fedez', 'place: 9']
-// console.log(fedez)
-const amadeus = ['tableName: Tavolo Vip', 'guestName: Amadeus', 'place: 10']
-// console.log(amadeus)
-const fiorello = ['tableName: Tavolo Vip', 'guestName: Fiorello', 'place: 11']
-// console.log(fiorello)
+// // creo gli array con i dati richiesti in forma di stringhe
+// const bradPitt = ['tableName: Tavolo Vip', 'guestName: Brad Pitt', 'place: 1']
+// // console.log(bradPitt)
+// const johnnyDepp = ['tableName: Tavolo Vip', 'guestName: Johnny Depp', 'place: 2']
+// // console.log(johnnyDepp)
+// const ladyGaga = ['tableName: Tavolo Vip', 'guestName: Lady Gaga', 'place: 3']
+// // console.log(ladyGaga)
+// const cristianoRonaldo = ['tableName: Tavolo Vip', 'guestName: Cristiano Ronaldo', 'place: 4']
+// // console.log(cristianoRonaldo)
+// const georginaRodrigue = ['tableName: Tavolo Vip', 'guestName: Georgina Rodriguez', 'place: 5']
+// // console.log(georginaRodrigue)
+// const chiaraFerragni = ['tableName: Tavolo Vip', 'guestName: Chiara Ferragni', 'place: 6']
+// // console.log(chiaraFerragni)
+// const georgeClooney = ['tableName: Tavolo Vip', 'guestName: George Clooney', 'place: 7']
+// // console.log(georgeClooney)
+// const amalClooney = ['tableName: Tavolo Vip', 'guestName: Amal Clooney', 'place: 8']
+// // console.log(amalClooney)
+// const fedez = ['tableName: Tavolo Vip', 'guestName: Fedez', 'place: 9']
+// // console.log(fedez)
+// const amadeus = ['tableName: Tavolo Vip', 'guestName: Amadeus', 'place: 10']
+// // console.log(amadeus)
+// const fiorello = ['tableName: Tavolo Vip', 'guestName: Fiorello', 'place: 11']
+// // console.log(fiorello)
 
-// inserisco gli array ospiti in un array che li contenga
-const listaOspiti = [bradPitt,johnnyDepp,ladyGaga,cristianoRonaldo,georginaRodrigue,chiaraFerragni,georgeClooney,amalClooney,fedez,amadeus,fiorello]
+// // inserisco gli array ospiti in un array che li contenga
+// const listaOspiti = [bradPitt,johnnyDepp,ladyGaga,cristianoRonaldo,georginaRodrigue,chiaraFerragni,georgeClooney,amalClooney,fedez,amadeus,fiorello]
+// // console.log(listaOspiti)
+
+// // creo ciclo for per convertile le stringhe in oggetti
+// let i
+// for (i=0; i<listaOspiti.length; i++){
+//     bradPitt.pop(); bradPitt.pop()
+//     bradPitt[0] = {tableName: 'Tavolo Vip',guestName: 'Brad Pitt',place: 1}
+//     // console.log(bradPitt)
+
+//     johnnyDepp.pop(); johnnyDepp.pop()
+//     johnnyDepp[0] = {tableName: 'Tavolo Vip',guestName: 'Johnny Depp',place: 2}
+//     // console.log(johnnyDepp)
+
+//     ladyGaga.pop(); ladyGaga.pop()
+//     ladyGaga[0] = {tableName: 'Tavolo Vip',guestName: 'Lady Gaga',place: 3}
+//     // console.log(ladyGaga)
+
+//     cristianoRonaldo.pop(); cristianoRonaldo.pop()
+//     cristianoRonaldo[0] = {tableName: 'Tavolo Vip',guestName: 'Cristiano Ronaldo',place: 4}
+//     // console.log(cristianoRonaldo)
+
+//     georginaRodrigue.pop(); georginaRodrigue.pop()
+//     georginaRodrigue[0] = {tableName: 'Tavolo Vip',guestName: 'Georgina Rodriguez',place: 5}
+//     // console.log(georginaRodrigue)
+
+//     chiaraFerragni.pop(); chiaraFerragni.pop()
+//     chiaraFerragni[0] = {tableName: 'Tavolo Vip',guestName: 'Chiara Ferragni',place: 6}
+//     // console.log(chiaraFerragni)
+
+//     georgeClooney.pop(); georgeClooney.pop()
+//     georgeClooney[0] = {tableName: 'Tavolo Vip',guestName: 'George Clooney',place: 7}
+//     // console.log(georgeClooney)
+
+//     amalClooney.pop(); amalClooney.pop()
+//     amalClooney[0] = {tableName: 'Tavolo Vip',guestName: 'Amal Clooney',place: 8}
+//     // console.log(amalClooney)
+
+//     fedez.pop(); fedez.pop()
+//     fedez[0] = {tableName: 'Tavolo Vip',guestName: 'Fedez',place: 9}
+//     // console.log(fedez)
+
+//     amadeus.pop(); amadeus.pop()
+//     amadeus[0] = {tableName: 'Tavolo Vip',guestName: 'Amadeus',place: 10}
+//     // console.log(amadeus)
+
+//     fiorello.pop(); fiorello.pop()
+//     fiorello[0] = {tableName: 'Tavolo Vip',guestName: 'Fiorello',place: 11}
+//     // console.log(fiorello)
+// }
+
 // console.log(listaOspiti)
 
-// creo ciclo for per convertile le stringhe in oggetti
-let i
-for (i=0; i<listaOspiti.length; i++){
-    bradPitt.pop(); bradPitt.pop()
-    bradPitt[0] = {tableName: 'Tavolo Vip',guestName: 'Brad Pitt',place: 1}
-    // console.log(bradPitt)
 
-    johnnyDepp.pop(); johnnyDepp.pop()
-    johnnyDepp[0] = {tableName: 'Tavolo Vip',guestName: 'Johnny Depp',place: 2}
-    // console.log(johnnyDepp)
+// * Soluzione usando il metodo array.map
 
-    ladyGaga.pop(); ladyGaga.pop()
-    ladyGaga[0] = {tableName: 'Tavolo Vip',guestName: 'Lady Gaga',place: 3}
-    // console.log(ladyGaga)
+// Creo gli array con i dati richiesti in forma di stringhe
+const listaOspiti = ['Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni',  'George Clooney', 'Amal Clooney', 'Fedez', 'Amadeus', 'Fiorello']
 
-    cristianoRonaldo.pop(); cristianoRonaldo.pop()
-    cristianoRonaldo[0] = {tableName: 'Tavolo Vip',guestName: 'Cristiano Ronaldo',place: 4}
-    // console.log(cristianoRonaldo)
+// Converto gli elementi stringa dell'array in elementi object con i dati richiesti
+const listaInvitati = listaOspiti.map((ospite,indice)=>({
+    tableName:`Tavolo Vip`,
+    guestName:ospite,
+    place:indice+1,
+}))
 
-    georginaRodrigue.pop(); georginaRodrigue.pop()
-    georginaRodrigue[0] = {tableName: 'Tavolo Vip',guestName: 'Georgina Rodriguez',place: 5}
-    // console.log(georginaRodrigue)
-
-    chiaraFerragni.pop(); chiaraFerragni.pop()
-    chiaraFerragni[0] = {tableName: 'Tavolo Vip',guestName: 'Chiara Ferragni',place: 6}
-    // console.log(chiaraFerragni)
-
-    georgeClooney.pop(); georgeClooney.pop()
-    georgeClooney[0] = {tableName: 'Tavolo Vip',guestName: 'George Clooney',place: 7}
-    // console.log(georgeClooney)
-
-    amalClooney.pop(); amalClooney.pop()
-    amalClooney[0] = {tableName: 'Tavolo Vip',guestName: 'Amal Clooney',place: 8}
-    // console.log(amalClooney)
-
-    fedez.pop(); fedez.pop()
-    fedez[0] = {tableName: 'Tavolo Vip',guestName: 'Fedez',place: 9}
-    // console.log(fedez)
-
-    amadeus.pop(); amadeus.pop()
-    amadeus[0] = {tableName: 'Tavolo Vip',guestName: 'Amadeus',place: 10}
-    // console.log(amadeus)
-
-    fiorello.pop(); fiorello.pop()
-    fiorello[0] = {tableName: 'Tavolo Vip',guestName: 'Fiorello',place: 11}
-    // console.log(fiorello)
-}
-
-console.log(listaOspiti)
+console.log(listaInvitati)
