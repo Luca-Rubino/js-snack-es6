315c9e5310e2a7eaf72522dca6c1bcda47698cde
Luca-Rubino <lucasrubino92@gmail.com>
2024-05-30 21:35:35 +0200

    Creare array di oggetti con le squadre da calcio

--- slack-3/script.js
+++ slack-3/script.js
@@ -27,7 +27,8 @@ const listaBici = [
         peso:8.75,
     },
 ]
-console.log(listaBici)
+
+// console.log(listaBici)
 
 // utilizzando destructuring per inserire in una costante i singoli oggetti
 const [tarmacSl8,trekDomane,cannondale,gravelTurbo,gravelDiverge] = listaBici
@@ -40,14 +41,19 @@ const elencoPesoBici = [] // lista con solo il peso di ogni singola bici
 
 listaBici.forEach((biciclette, i) => {
     const {peso} = listaBici[i];
-    console.log({peso});
+
+    // console.log({peso});
+
     elencoPesoBici.push(peso)
 });
-console.log(elencoPesoBici);
+
+// console.log(elencoPesoBici);
 
 // utilizzo il metodo filter per ottenere il parametro peso con il valore minore
 const biciPiuLeggera = listaBici.filter((biciclette) => biciclette.peso === Math.min(...elencoPesoBici));
-console.log(biciPiuLeggera);
+
+// console.log(biciPiuLeggera);
+
 // stampo in console qual'è la bici più leggera usando il console.log con template literal
 console.log(`La bici piu' leggera e': ${biciPiuLeggera[0].nome} e pesa: ${biciPiuLeggera[0].peso} kg`);
 
@@ -56,6 +62,33 @@ console.log(`La bici piu' leggera e': ${biciPiuLeggera[0].nome} e pesa: ${biciPi
 // > Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 // > Generare numeri random al posto degli 0 nelle proprietà: punti fatti e falli subiti.Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 
+const squadre = [
+    {
+        nome: 'Juventus',
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: 'Milan',
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: 'Atalanta',
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: 'Genova',
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: 'Bologna',
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+]
 
 
 // BONUS 1
